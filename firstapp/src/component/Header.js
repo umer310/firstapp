4b5a62f7408077ef5dfe88599c4f877162ccaf8a
umer farooq <umer.ataki@gmail.com>
2020-07-13 17:10:28 +0500

    initila commit

---
+++ firstapp/src/component/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// function Header(){
+//     return(
+//         <h1>i am header</h1>
+//     );
+// }
+const Header = () => <h1>i am heasr</h1>
+
+export default Header;
